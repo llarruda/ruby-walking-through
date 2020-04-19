4806f0a875dbb1604fdecc118dec947d2a999020
llarruda <leandroarruda1977@gmail.com>
2020-04-18 22:30:58 -0300

    Add first sample of basic level of Ruby

---
+++ basic_io_interactions_001.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+#
+#  Ruby Basic Syntax - samples of I/O operations and basic string usage
+#
+#  created by @llarruda
+#  Sat 18 Apr 2020 10:21:58
+#
+
+puts 'Hello Ruby World!'
+
+print 'Enter your name: '
+
+name = gets.chomp
+
+puts "Your journey with Ruby, #{name}, certainly, will be more fun!"
