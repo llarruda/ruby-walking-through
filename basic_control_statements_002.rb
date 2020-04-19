d78ed415177d5ada283480b672a6068f57782890
llarruda <leandroarruda1977@gmail.com>
2020-04-18 23:11:35 -0300

    Add second sample of basic level of Ruby

---
+++ basic_control_statements_002.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+#
+#  Ruby Basic Syntax - samples of I/O operations and basic string usage
+#
+#  created by @llarruda
+#  Sat 18 Apr 2020 11:05:12
+#
+
+puts 'Enter a integer: '
+
+number = gets.chomp.to_i
+
+if number < 10
+  puts "#{number} is greater than 10."
+else
+  puts "#{number} is less than 10."
+end
+
+while number < 10
+  puts "hey #{number}"
+  number = number + 1
+end
+
+# statement modifiers are useful when the body of statement is only a single expression
+# behaves as do-while, at least once the subtraction will be realize
+puts number = number - 5 while number > 10
